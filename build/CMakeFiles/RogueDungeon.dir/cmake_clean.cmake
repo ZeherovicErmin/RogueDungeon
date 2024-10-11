1b53fca16272930d70e356f9e506d7af9fa73169
Ermin <ZeherovicErmin@gmail.com>
2024-10-10 23:30:34 -0400

    Creating a window

---
+++ build/CMakeFiles/RogueDungeon.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RogueDungeon.dir/main.cpp.o"
+  "CMakeFiles/RogueDungeon.dir/main.cpp.o.d"
+  "RogueDungeon"
+  "RogueDungeon.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RogueDungeon.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
